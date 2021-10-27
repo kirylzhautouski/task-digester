f309827d0217731bbf998026c6a06682f815bed8
Kiryl Zhautouski <kirilzh2000@gmail.com>
2021-10-27 15:52:45 +0300

    Bootstrap Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "com.kirylzhautouski.task-digester"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
